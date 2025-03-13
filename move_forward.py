6322135c446083f6045b16e5a142bb9ccc036045
Daniall <s235070@dtu.dk>
2025-03-13 14:58:46 +0000

    Test of Moving Forward

---
+++ move_forward.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import MoveTank, OUTPUT_D, OUTPUT_A
+from time import sleep
+
+# Initialize a "tank" drive with large motors on outputs D and A
+tank_drive = MoveTank(OUTPUT_D, OUTPUT_A)
+
+# Move forward at speed=50 for 2 seconds
+tank_drive.on_for_seconds(left_speed=100, right_speed=100, seconds=5)
+
+# Stop motors
+tank_drive.off()
+
