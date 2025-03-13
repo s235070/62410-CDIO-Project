bccc1d3be26e574aa49a74cd5a5120e3f9827515
Daniall <S235070@student.dtu.dk>
2025-03-13 19:36:35 +0100

    added test for ir and wheels

--- testOfComponents/brianIR.py
+++ testOfComponents/brianIR.py
@@ -2,43 +2,40 @@
 
 import time
 from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
-from ev3dev2.sensor.lego import InfraredSensor
 
-# Initialize motors
+# Initialize the motors
 tank = MoveTank(OUTPUT_B, OUTPUT_C)
 
-# If you want IR for checking obstacles, uncomment the lines below:
-# ir = InfraredSensor('in1')
-# ir.mode = 'IR-PROX'
-
-FORWARD_TIME = 30  # Drive forward for 30s
-CIRCLE_TIME  = 10  # Drive in a circle for 10s
+# Durations (seconds)
+FORWARD_TIME = 30
+CIRCLE_TIME  = 10
 
 try:
     # 1) Drive straight for 30s
-    #    Left motor = -30, Right motor = +30
     print("Driving straight for {} seconds...".format(FORWARD_TIME))
     start_time = time.time()
-    while (time.time() - start_time) < FORWARD_TIME:
-        tank.on(left_speed=-30, right_speed=30)
+    while time.time() - start_time < FORWARD_TIME:
+        # According to your code, "straight forward" is (-30, -30)
+        tank.on(left_speed=-30, right_speed=-30)
         time.sleep(0.1)
     tank.off()
 
     # 2) Drive in a circle for 10s
-    #    Example circle: Left motor = -30, Right motor = -10
     print("Driving in a circle for {} seconds...".format(CIRCLE_TIME))
     start_time = time.time()
-    while (time.time() - start_time) < CIRCLE_TIME:
-        tank.on(left_speed=-30, right_speed=-10)
+    while time.time() - start_time < CIRCLE_TIME:
+        # Circle drive is (-30, +30)
+        tank.on(left_speed=-30, right_speed=30)
         time.sleep(0.1)
     tank.off()
 
-    # 3) Drive straight indefinitely until Ctrl + C
-    print("Now driving straight indefinitely. Press Ctrl+C to stop.")
+    # 3) Drive straight indefinitely
+    print("Now driving straight indefinitely. Press CTRL+C to stop.")
     while True:
-        tank.on(left_speed=-30, right_speed=30)
+        # Again, "straight" is (-30, -30)
+        tank.on(left_speed=-30, right_speed=-30)
         time.sleep(0.1)
 
 except KeyboardInterrupt:
-    print("Caught Ctrl+C, stopping motors now.")
+    print("Stopping robot now.")
     tank.off()
