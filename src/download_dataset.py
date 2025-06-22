85534fc5fd36e85d0382dc55e4638233202e648b
Daniall <S235070@student.dtu.dk>
2025-06-22 16:20:26 +0200

    added changes

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="h6m7QEqT6Po9FCmTAPUH")
-project = rf.workspace("balldetection-z7gmn").project("ev3-ld4mh")
-version = project.version(1)
+project = rf.workspace("balldetection-z7gmn").project("ball-detection-mobgq")
+version = project.version(6)
 dataset = version.download("yolov8")
