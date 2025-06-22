a0d868dfb85384a137e6d935f74d893e319d3ab8
Abdullah Hassan <145469232+Abdullah1371@users.noreply.github.com>
2025-06-22 15:08:14 +0200

    added track ev3

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,7 +1,5 @@
 from roboflow import Roboflow
-
-# Brug din egen API-nøgle og workspace/projekt/version
 rf = Roboflow(api_key="h6m7QEqT6Po9FCmTAPUH")
-project = rf.workspace("balldetection-z7gmn").project("ball-detection-mobgq")
-version = project.version(2)  # Brug v2
+project = rf.workspace("balldetection-z7gmn").project("ev3-ld4mh")
+version = project.version(1)
 dataset = version.download("yolov8")
