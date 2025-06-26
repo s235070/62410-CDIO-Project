b92ca526ed5b180faa7c89eef13d94075f96085f
Daniall Mudasar <145471877+s235070@users.noreply.github.com>
2025-06-27 01:30:07 +0200

    download_dataset.py

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -1,5 +1,5 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="h6m7QEqT6Po9FCmTAPUH")
-project = rf.workspace("balldetection-z7gmn").project("track-system")
+rf = Roboflow(api_key="xxxxxxxxxxxxxxxxxxxxxxxxx")
+project = rf.workspace("xxxxxxxxxxxxxxxxxxxxxx").project("xxxxxxxxxxxxxxxxxxxxxxx")
 version = project.version(6)
 dataset = version.download("yolov8")
