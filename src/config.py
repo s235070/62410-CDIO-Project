249d6ca4c502a9c2b7863922abef4df33b87c8b5
YH313313 <144796401+YH313313@users.noreply.github.com>
2025-06-26 20:00:15 +0200

    Rettelse af track

--- src/config.py
+++ src/config.py
@@ -14,5 +14,5 @@ WARP_WIDTH = 640   # Output resolution of warped image
 WARP_HEIGHT = 480
 
 # ====== EV3 SETTINGS ======
-EV3_IP = "172.20.10.6"
+EV3_IP = "172.20.10.6" # Baseret på vores mobildata. 
 EV3_USER = "robot"
